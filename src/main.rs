b2f3cbcf562f88823177a317ea512ab8dc09271f
kekeimiku <kekelanact@gmail.com>
2022-01-26 23:10:53 +0800

    '12121'

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,3 @@
-use std::{thread, time::Duration};
-
-use rand::Rng;
 use windows_sys::Win32::{
     Graphics::Gdi::{BitBlt, GetDC, ReleaseDC, SRCCOPY},
     UI::WindowsAndMessaging::{GetSystemMetrics, SM_CXSCREEN, SM_CYSCREEN},
@@ -11,21 +8,19 @@ fn main() {
         let width = GetSystemMetrics(SM_CXSCREEN);
         let height = GetSystemMetrics(SM_CYSCREEN);
         let (mut x, mut y, mut w, mut h);
-        let mut rng = rand::thread_rng();
 
         loop {
-            thread::sleep(Duration::from_millis(10));
             let hdc = GetDC(0);
 
-            x = rng.gen::<i32>() % (width * 5 / 4) - width / 4;
-            y = rng.gen::<i32>() % (height * 5 / 4) - height / 4;
-            w = rng.gen::<i32>() % width / 2;
-            h = rng.gen::<i32>() % height / 2;
+            x = fastrand::i32(1..3000) % (width * 5 / 4) - width / 4;
+            y = fastrand::i32(1..3000) % (height * 5 / 4) - height / 4;
+            w = fastrand::i32(1..3000) % width / 2;
+            h = fastrand::i32(1..3000) % height / 2;
 
             BitBlt(
                 hdc,
-                x + rng.gen::<i32>() % 3 - 1,
-                y + rng.gen::<i32>() % 5,
+                x + fastrand::i32(1..3000) % 3 - 1,
+                y + fastrand::i32(1..3000) % 5,
                 w,
                 h,
                 hdc,
